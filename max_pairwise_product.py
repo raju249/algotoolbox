439e335392a3caabb1258441a4f3032c5f3e14b1
Rajendra <rajendrakadam249@gmail.com>
2016-07-26 20:24:04 +0530

    Max Product week 1

---
+++ max_pairwise_product.py
@@ -0,0 +1,16 @@
+# Uses python3
+n = int(input())
+a = [int(x) for x in input().split(" ")]
+assert(len(a) == n)
+
+result = 0
+
+"""for i in range(0, n):
+    for j in range(i+1, n):
+        if a[i]*a[j] > result:
+            result = a[i]*a[j]"""
+max1 = max(a)
+a.remove(max1)
+max2  = max(a)
+
+print(max1*max2)
