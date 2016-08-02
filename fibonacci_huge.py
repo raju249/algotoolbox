0e72f0b343a5feb71e4a98b31d17828ec96dc19c
Rajendra <rajendrakadam249@gmail.com>
2016-08-02 22:20:28 +0530

    Week 2

---
+++ fibonacci_huge.py
@@ -0,0 +1,11 @@
+# Uses python3
+import sys
+
+def get_fibonacci_huge(n, m):
+    # write your code here
+    return 0
+
+if __name__ == '__main__':
+    input = sys.stdin.read();
+    n, m = map(int, input.split())
+    print(get_fibonacci_huge(n, m))
