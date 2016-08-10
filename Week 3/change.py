3fa90890e3a6662a92df20ae69185917ad31e038
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-08-10 23:11:15 +0530

    Change Problem

--- Week 3/change.py
+++ Week 3/change.py
@@ -3,7 +3,18 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    coins = 0
+    while m is not 0:
+    	if m >= 10:
+    		coins += 1
+    		m = m - 10
+    	elif m >= 5:
+    		coins += 1
+    		m = m - 5
+    	elif m >= 1:
+    		coins += 1
+    		m = m - 1
+    return coins
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
