44862c61f73ceccc137421b28bce17c4c85d26c1
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-08-13 20:33:00 +0530

    Largest Number final prob

--- Week 3/largest_number.py
+++ Week 3/largest_number.py
@@ -1,17 +1,22 @@
-#Uses python3
+#Uses python2
 
 import sys
 
+def compare(a,b):
+    str1 = str(a) + str(b)
+    str2 = str(b) + str(a)
+    if (int(str1) < int(str2)):
+    	return 1
+    else:
+    	return -1
+
 def largest_number(a):
     #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+    res = sorted(a, cmp = compare)
+    return "".join(res)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
     print(largest_number(a))
-    
