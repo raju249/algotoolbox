162ac6b88e4e4e9b6ef4410db65f0594e045f82b
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-08-13 13:05:08 +0530

    Summands Greedy

--- Week 3/different_summands.py
+++ Week 3/different_summands.py
@@ -4,6 +4,17 @@ import sys
 def optimal_summands(n):
     summands = []
     #write your code here
+    n1 = n
+    for i in range(1,n + 1):
+    	n1 = n1 - i
+    	if n1 == 0:
+    		summands.append(i)
+    		break
+    	if n1 <= i:
+    		summands.append(n1 + i)
+    		break
+    	else:
+    		summands.append(i)
     return summands
 
 if __name__ == '__main__':
