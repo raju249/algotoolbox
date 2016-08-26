7a66832ed07f659b42efcbc2169d7559dfd4f51f
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-08-26 21:40:56 +0530

    Week 4 binary search

--- Week 4/binary_search.py
+++ Week 4/binary_search.py
@@ -1,9 +1,21 @@
 # Uses python3
 import sys
-
+import math
 def binary_search(a, x):
-    left, right = 0, len(a)
+    low, high = 0, len(a) - 1
     # write your code here
+    return binary(a,x,low,high)
+
+def binary(a,x,low,high):
+    if high < low:
+        return -1
+    mid = math.floor(low + (high - low) // 2)
+    if x == a[mid]:
+        return mid
+    elif x < a[mid]:
+        return binary(a,x,low,mid - 1)
+    else:
+        return binary(a,x,mid + 1,high)
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +31,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
