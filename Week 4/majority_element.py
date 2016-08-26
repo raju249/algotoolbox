111d59f82e08562221fda0af166836f2c02339a6
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-08-26 22:02:14 +0530

    Week 4 majority using O(n) algorithm

--- Week 4/majority_element.py
+++ Week 4/majority_element.py
@@ -2,11 +2,23 @@
 import sys
 
 def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
+    count = 0
+    maj = -1
+    l = len(a)
+    for i in range(l):
+        if count == 0:
+            maj = a[i]
+        if a[i] == maj:
+            count += 1
+        else:
+            count -= 1
+
+    count = 0
+    for i in range(l):
+        if a[i] == maj:
+            count += 1
+        if count > l//2:
+            return 1
     return -1
 
 if __name__ == '__main__':
