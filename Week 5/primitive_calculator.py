cb1ea95d123a37d587648470616a58e1d835df07
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-12-10 17:27:44 +0530

    Primitive calculator

--- Week 5/primitive_calculator.py
+++ Week 5/primitive_calculator.py
@@ -13,9 +13,31 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
+def dynamic_sequence(n):
+    sequence = [sys.maxsize] * (n + 1)
+    sequence[1] = 0
+    for i in range(2, n + 1):
+        sequence[i] = 1 + sequence[i - 1]
+        if i % 2 == 0:
+            sequence[i] = min(sequence[i], 1 + sequence[i // 2])
+        if i % 3 == 0:
+            sequence[i] = min(sequence[i], 1 + sequence[i // 3])
+    step = sequence[n]
+    a = [-1]*(step + 1)
+    a[0] = n
+    a[step] = 1
+    for i in range(1,step):
+        if a[i - 1] % 2 == 0 and sequence[a[i - 1] // 2] + 1 == sequence[a[i - 1]]:
+            a[i] = a[i - 1] // 2
+        elif a[i - 1] % 3 == 0 and sequence[a[i - 1] // 3] + 1 == sequence[a[i - 1]]:
+            a[i] = a[i - 1] // 3
+        else:
+            a[i] = a[i - 1] - 1
+    return reversed(a)
+
 input = sys.stdin.read()
 n = int(input)
-sequence = list(optimal_sequence(n))
+sequence = list(dynamic_sequence(n))
 print(len(sequence) - 1)
 for x in sequence:
     print(x, end=' ')
