6ebb83e04ddc12bfb4064f9e7dcb593f72458245
Rajendra Kadam <rajendrakadam249@gmail.com>
2016-12-11 17:27:18 +0530

    LCS

--- Week 5/lcs3.py
+++ Week 5/lcs3.py
@@ -1,23 +1,72 @@
-#Uses python3
-
+# Uses python3
 import sys
+def calc_cache_pos(strings, indexes):
+    factor = 1
+    pos = 0
+    for s, i in zip(strings, indexes):
+        pos += i * factor
+        factor *= len(s)
+    return pos
 
-def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+def lcs_back(strings, indexes, cache):
+    if -1 in indexes:
+        return ""
+    match = all(strings[0][indexes[0]] == s[i]
+                for s, i in zip(strings, indexes))
+    if match:
+        new_indexes = [i - 1 for i in indexes]
+        result = lcs_back(strings, new_indexes, cache) + strings[0][indexes[0]]
+    else:
+        substrings = [""] * len(strings)
+        for n in range(len(strings)):
+            if indexes[n] > 0:
+                new_indexes = indexes[:]
+                new_indexes[n] -= 1
+                cache_pos = calc_cache_pos(strings, new_indexes)
+                if cache[cache_pos] is None:
+                    substrings[n] = lcs_back(strings, new_indexes, cache)
+                else:
+                    substrings[n] = cache[cache_pos]
+        result = max(substrings, key=len)
+    cache[calc_cache_pos(strings, indexes)] = result
+    return result
 
+def lcs(strings):
+    """
+    >>> lcs(['666222054263314443712', '5432127413542377777', '6664664565464057425'])
+    '54442'
+    >>> lcs(['abacbdab', 'bdcaba', 'cbacaa'])
+    'baa'
+    """
+    if len(strings) == 0:
+        return ""
+    elif len(strings) == 1:
+        return strings[0]
+    else:
+        cache_size = 1
+        for s in strings:
+            cache_size *= len(s)
+        cache = [None] * cache_size
+        indexes = [len(s) - 1 for s in strings]
+        return lcs_back(strings, indexes, cache)
+        
+        
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     an = data[0]
     data = data[1:]
     a = data[:an]
+    a = ''.join(str(x) for x in a)
     data = data[an:]
     bn = data[0]
     data = data[1:]
     b = data[:bn]
+    b = ''.join(str(x) for x in b)
     data = data[bn:]
     cn = data[0]
     data = data[1:]
     c = data[:cn]
-    print(lcs3(a, b, c))
+    c = ''.join(str(x) for x in c)
+    print(len(lcs([a,b,c])))
+    #lcs(['abacbdab', 'bdcaba', 'cbacaa'])
